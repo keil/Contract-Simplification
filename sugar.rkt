a9c61981cc20a52c16e845398b328aafaf66b5e5
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-04 10:16:50 +0000

    snapshot
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26099 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- sugar.rkt
+++ sugar.rkt
@@ -35,7 +35,7 @@
   ((e f g) .... (assert e C))
   
   ;; evaluation context
-  ((E F G) .... (assert E C) (v @ (eval E)))
+  ((E F G) .... (assert E C) (E @ C) (v @ (eval E)))
 )
 
 ;; test 
@@ -55,6 +55,19 @@
 ;;| | \ \  __/ (_| | |_| | (__| |_| | (_) | | | |
 ;;|_|  \_\___|\__,_|\__,_|\___|\__|_|\___/|_| |_|
 
+;(define-metafunction λ_C
+;  subst : x v e -> e
+;  [(subst x v x) v]
+;  [(subst x v y) y]
+;  [(subst x v c) c]
+;  [(subst x v (op e f)) (op (subst x v e) (subst x v f))]
+;  [(subst x v (λ x e)) (λ x e)]
+;  [(subst x v (λ y e)) (λ y (subst x v e))]
+;  [(subst x v (e f)) ((subst x v e) (subst x v f))]
+;  [(subst any) (subst x v e)]
+;)
+
+
 (define λ_C-reduction
   (extend-reduction-relation λ_J-reduction
    λ_C
@@ -66,19 +79,19 @@
         (in-hole E (v @ (eval (e v))))
         "Flat"
    )
-   (--> (in-hole E (v @ (eval 1)))
+   (--> (in-hole E (v @ (eval w)))
         (in-hole E v)
         "Unit"
+        (side-condition (> (term w) 0))
    )
    (--> (in-hole E (v @ (eval 0)))
-        (in-hole E blame)
+        blame
         "Blame"
    )
-   
    (--> (in-hole E ((v @ (C → D)) w))
         (in-hole E ((v (w @ C)) @ D))
         "Function"
-   )
+   )  
 ))
 
 ;;; extebd dubst
@@ -88,19 +101,40 @@
 (define False (term (flat (λ x 0))))
 
 (define Pos (term (flat (λ x (> x 0)))))
-(define Nat (term (flat (λ x 0))))
+(define Nat (term (flat (λ x (+ (> x 0) (= x 0))))))
 
 ;; test 
 ;(traces λ_C-reduction (term ((+ 1 2) @ (flat (λ x 1)))))
 ;(traces λ_C-reduction (term ((+ 1 2) @ ,false)))
 
-(traces λ_C-reduction (term (((λ x 1) @ (,true → ,true)) 1)))
+;(traces λ_C-reduction (term ((assert (λ x (+ x 1)) (,Nat → ,Nat)) 1)))
+
+;(traces λ_C-reduction (term ((assert (λ x (+ x 1)) (,Pos → ,Pos)) 0)))
+;(traces λ_C-reduction (term ((assert (λ x (- x 1)) (,Pos → ,Pos)) 1)))
+
+;(traces λ_C-reduction (term (((assert (λ x (λ y (+ x y))) (,Pos → (,Pos → ,Pos))) 1) 1)))
 
 
-;(traces let-reduction (term xx))
-;(traces let-reduction (term (1 + 2)))
-;(traces let-reduction (term xx))
-;(traces let-reduction (term (let x = (1 + 1) in (2 + 2))))
-;(traces let-reduction (term (let x = (1 + 1) in (x + 2))))
-;(traces let-reduction (term (let x = 1 in (x + 1))))
-;(traces let-reduction (term (let x = 1 in (let y = 1 in (x + y)))))
+(traces λ_C-reduction (term
+                       ((λ f (f 1)) 
+                        (assert (λ x (+ x 1)) (,Pos → ,Pos))
+                        ) 
+))
+
+
+;(traces λ_C-reduction (term (((assert (λ f (λ x ((f x) 1))) ((,Pos → (,Pos → ,Pos)) → (,Pos → ,Pos))) 
+;                             (λ x (λ y (+ x y)))
+;                             ) 1)
+;                             ))
+
+
+
+;(traces λ_C-reduction (term (
+;                             (
+;                              (assert (λ f (λ x (f x 1))) ((,Pos → (,Pos → ,Pos)) → (,Pos → ,Pos)))
+;                              (λ x (λ y (+ x y)))
+;                              ) 1
+;                             )))
+                             
+
+
