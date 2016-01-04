050be421103f7dd8e65951d9c84565a639f2fd4d
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-04 08:02:26 +0000

    sznc
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26095 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ test.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(require redex)
+(require "lj.rkt")
+
+;; test
+;(redex-match λ_J e (term 1))
+;(redex-match λ_J e (term x))
+
+;(redex-match λ_J e (term (+ 1 1)))
+;(redex-match λ_J e (term (* 1 1)))
+
+;(redex-match λ_J e (term ((λ x (+ x 1)) 1)))
+;(redex-match λ_J e (term (((λ x (λ y (+ x y))) 1) 1)))
+;(redex-match λ_J e (term ((λ x (x 1)) (λ x x))))
+
+;; test
+;(traces λ_J-reduction (term 1))
+;(traces λ_J-reduction (term x))
+
+;(traces λ_J-reduction (term (+ 1 1)))
+;(traces λ_J-reduction (term (* 1 1)))
+
+;(traces λ_J-reduction (term (λ x 1)))
+;(traces λ_J-reduction (term ((λ x 1) 1)))
+
+;(traces λ_J-reduction (term ((λ x (+ x 1)) 1)))
+;(traces λ_J-reduction (term (((λ x (λ y (+ x y))) 1) 1)))
+;(traces λ_J-reduction (term ((λ x (x 1)) (λ x x))))
