cbd5c9863e7d3cafa3c09ef708d0a04970d2b1bc
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-05 13:35:06 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26120 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- test.rkt
+++ test.rkt
@@ -1,7 +1,5 @@
 #lang racket
 (require redex)
-(require "lj.rkt")
-(require "lcon.rkt")
 
 #|
  _____       _      
@@ -10,65 +8,3 @@
   |_|\___/__/\__/__/
                     
 |#
-
-;; Test λ_J/ Syntax
-(redex-match λ_J e (term 1))
-(redex-match λ_J e (term x))
-
-(redex-match λ_J e (term (+ 1 1)))
-(redex-match λ_J e (term (* 1 1)))
-
-(redex-match λ_J e (term ((λ x (+ x 1)) 1)))
-(redex-match λ_J e (term (((λ x (λ y (+ x y))) 1) 1)))
-(redex-match λ_J e (term ((λ x (x 1)) (λ x x))))
-
-;; Test λ_J/ Reduction
-(test-->> λ_J-reduction (term 1) (term 1))
-(test-->> λ_J-reduction (term x) (term x))
-
-(test-->> λ_J-reduction (term (+ 1 1)) (term 2))
-(test-->> λ_J-reduction (term (* 1 1)) (term 1))
-
-(test-->> λ_J-reduction (term (- 1 1)) (term 0))
-(test-->> λ_J-reduction (term (/ 1 1)) (term 1))
-(test-->> λ_J-reduction (term (> 1 2)) (term 0))
-(test-->> λ_J-reduction (term (< 1 2)) (term 1))
-(test-->> λ_J-reduction (term (= 1 2)) (term 0))
-(test-->> λ_J-reduction (term (= 1 1)) (term 1))
-
-(test-->> λ_J-reduction (term (λ x 1)) (term (λ x 1)))
-(test-->> λ_J-reduction (term ((λ x 1) 1)) (term 1))
-
-(test-->> λ_J-reduction (term ((λ x (+ x 1)) 1)) (term 2))
-(test-->> λ_J-reduction (term (((λ x (λ y (+ x y))) 1) 1)) (term 2))
-(test-->> λ_J-reduction (term ((λ x (x 1)) (λ x x))) (term 1))
-
-;; Test λCon/ Syntax
-(redex-match λCon e (term (assert 1 (flat 1))))
-(redex-match λCon e (term (assert (+ 1 2) (flat 1))))
-
-(redex-match λCon e (term (assert 1 (flat (+ 1 1)))))
-(redex-match λCon e (term (assert (+ 1 2) (flat (+ 1 1)))))
-
-(redex-match λCon e (term (assert (λ x (+ x 1)) ((flat 1) → (flat 1)))))
-(redex-match λCon e (term ((assert (λ x (+ x 1)) ((flat 1) → (flat 1))) 1)))
-
-(redex-match λCon e (term ((λ x 1) blame)))
-
-;; Test λCon/ Reduction
-(test-->> λCon-reduction (term ((+ 1 2) @ (flat (λ x 1)))) (term 3))
-(test-->> λCon-reduction (term ((+ 1 2) @ ,Any)) (term 3))
-(test-->> λCon-reduction (term ((+ 1 2) @ ,Blame)) (term blame))
-
-(test-->> λCon-reduction (term ((assert (λ x (+ x 1)) (,Nat → ,Nat)) 1)) (term 2))
-
-(test-->> λCon-reduction (term ((assert (λ x (+ x 1)) (,Pos → ,Pos)) 0)) (term blame)) 
-(test-->> λCon-reduction (term ((assert (λ x (- x 1)) (,Pos → ,Pos)) 1)) (term blame))
-
-(test-->> λCon-reduction (term (((assert (λ x (λ y (+ x y))) (,Pos → (,Pos → ,Pos))) 1) 1)) (term 2))
-
-(test-->> λCon-reduction (term ((λ f (f 1)) (assert (λ x (+ x 1)) (,Pos → ,Pos)))) (term 2))
-
-(test-->> λCon-reduction (term (((assert (λ plus (λ x ((plus x) 1))) ((,Pos → (,Pos → ,Pos)) → (,Pos → ,Pos))) (λ x (λ y (+ x y)))) 1)) (term 2))
-
-(test-results)
