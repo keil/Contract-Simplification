c90a0dc78b4d1b23b758341ad1d244205397498d
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-05 14:43:28 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26123 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ commit:
 	svn ci -m "sync"
 
 tag:
-	svn cp . ../../tags/Sugar_$(shell date +%Y%m%d)$(name)
-	svn ci ../../tags/Sugar_$(shell date +%Y%m%d)$(name)
+	svn cp . ../../tags/Sugar_$(shell date +%Y%m%d)-$(name)
+	svn ci ../../tags/Sugar_$(shell date +%Y%m%d)-$(name) -m "add new tag"
 
 clean:
 	rm -rf `svn propget svn:ignore`
--- sugar.rkt
+++ sugar.rkt
@@ -1,7 +1,9 @@
 #lang racket
 (require redex)
+
 (require "lj.rkt")
 (require "lcon.rkt")
+(require "contracts.rkt")
 
 (provide (all-defined-out))
 
@@ -18,39 +20,19 @@
                                                                   
 |#
 
-(define-extended-language λCon2 λCon
+(define-extended-language λCon-Baseline λCon
   
-  ;; terms (final terms)
-  ((r s t) 
-   c (λ x r) ;; values
-   ((λ x e) @ Q) ;; values 
-   x (op r s) (r s) (x @ C) 
-           (assert x C) (assert (λ x r) Q)
-           )
-  
-  ;; reduction context
-  ((R S T) 
-   hole
-   (λ x R)
-   (op R e)
-   (op r R)
-   (R e)
-   (r R)
-   (assert R C)
-   (R @ C)
-   )
+  ;; Baseline Terms (Final Terms)
+  ((S T) K x (λ x T) (S T) (op T ...) (x @ I) ((λ x M) @ Q))
+           
+  ;; Baseline Context
+  ((A B) hole (λ x B) (op S ... B M ...) (B M) (S B) (B @ C))
 )
 
-;; predicates
-(define done? (redex-match? λCon2 r))
+;; TODO
+;; every  is also an λCon-term
 
-(done? (term (+ 1 2)))
-(done? (term ((λ x (+ x 1)) 1)))
-(done? (term ((assert (λ x (+ x 1)) (,Nat → ,Nat)) 1)))
 
-(done? (term ((λ x (+ x 1)) (assert 1 ,Nat))))
-(done? (term ((λ x (+ x (assert 1 ,Nat))) 1)))
-(done? (term ((λ x (+ (assert x ,Nat) 1)) 1)))
 
 #|
  ___        _         _   _          
@@ -62,37 +44,96 @@
 
 (define Baseline-reduction
   (reduction-relation
-   λCon2
-   (--> (in-hole R (v @ (flat e)))
-        (in-hole R ,(car (apply-reduction-relation* λCon-reduction (term (v @ (flat e))))))
-        "Red-Flat"
+   λCon-Baseline
+   
+   (--> (in-hole B (V @ I))
+        (in-hole B ,(car (apply-reduction-relation* λCon-reduction (term (V @ I)))))
+        "Baseline-Flat"
    )
-   (--> (in-hole R (assert v I))
-        (in-hole R ,(car (apply-reduction-relation* λCon-reduction (term (assert v I)))))
-        "Red-2-Flat"
-   )
-   (--> (in-hole R (assert c (C → D)))
-        (in-hole R c)
-        "Red-Function"
+;   (--> (in-hole R (assert v I))
+;        (in-hole R ,(car (apply-reduction-relation* λCon-reduction (term (assert v I)))))
+;        "Red-2-Flat"
+;   )
+   (--> (in-hole B (K @ Q))
+        (in-hole B K)
+        "Baseline-Function"
    )
 ))
 
-;(apply-reduction-relation* λCon-reduction (term (1 @ (flat (λ x 1)))))
-;(apply-reduction-relation* λCon-reduction (term (assert 1 (flat (λ x 1)))))  
+(test-->> Baseline-reduction (term ((1 @ ,Nat) @ ,Nat)) (term 1))
+(test-->> Baseline-reduction (term ((0 @ ,Nat) @ ,Nat)) (term 0))
 
-(test-->> Baseline-reduction (term (assert (assert 1 ,Nat) ,Nat)) (term 1))
-(test-->> Baseline-reduction (term (assert (assert 0 ,Nat) ,Nat)) (term 0))
 ;(test-->> Baseline-reduction (term (assert (assert 0 ,Pos) ,Nat)) (term blmae))
-(test-->> Baseline-reduction (term (assert (assert 0 ,Nat) ,Pos)) (term blame))
+(test-->> Baseline-reduction (term ((0 @ ,Nat) @ ,Pos)) (term blame))
 
-(test-->> Baseline-reduction (term ((λ x (+ x 1)) (assert 1 ,Nat))) (term ((λ x (+ x 1)) 1)))
-(test-->> Baseline-reduction (term ((λ x (+ x (assert 1 ,Nat))) 1)) (term ((λ x (+ x 1)) 1)))
+(test-->> Baseline-reduction (term ((λ x (+ x 1)) (1 @ ,Nat))) (term ((λ x (+ x 1)) 1)))
+(test-->> Baseline-reduction (term ((λ x (+ x (1 @ ,Nat))) 1)) (term ((λ x (+ x 1)) 1)))
 
-(test-->> Baseline-reduction (term ((λ x (+ (assert x ,Nat) 1)) 1)) (term ((λ x (+ (assert x ,Nat) 1)) 1)))
+(test-->> Baseline-reduction (term ((λ x (+ (x @ ,Nat) 1)) 1)) (term ((λ x (+ (x @ ,Nat) 1)) 1)))
  
-(test-->> Baseline-reduction (term (assert 1 (,Nat → ,Nat))) (term 1)) 
-(test-->> Baseline-reduction (term (assert x (,Nat → ,Nat))) (term (assert x (,Nat → ,Nat))))
+(test-->> Baseline-reduction (term (1 @ (,Nat → ,Nat))) (term 1)) 
+(test-->> Baseline-reduction (term (x @ (,Nat → ,Nat))) (term (x @ (,Nat → ,Nat))))
 
-(traces Baseline-reduction (term (assert (assert 0 ,Pos) ,Nat)))
+;(traces Baseline-reduction (term (assert (assert 0 ,Pos) ,Nat)))
 ;(traces Baseline-reduction (term ((λ x (+ x 1)) (assert 1 ,Nat))))
 ;(traces Baseline-reduction (term ((λ x (+ x (assert 1 ,Nat))) 1)))
+
+
+
+
+(define done? (redex-match? λCon-Baseline S))
+
+; TODO, other test relation
+(done? (term (+ 1 2)))
+(done? (term ((λ x (+ x 1)) 1)))
+(done? (term (((λ x (+ x 1)) @ (,Nat → ,Nat)) 1)))
+
+(done? (term ((λ x (+ x 1)) (1 @ ,Nat))))
+(done? (term ((λ x (+ x (1 @ ,Nat))) 1)))
+(done? (term ((λ x (+ (x @ ,Nat) 1)) 1)))
+
+
+(define
+  (reduce M)
+  (car (apply-reduction-relation* Baseline-reduction M)))
+
+(define
+  (evaluate M)
+  (car (apply-reduction-relation* λCon-reduction M)))
+
+(define
+  (compare M)
+  (eq? (evaluate (reduce M)) (evaluate M)))
+
+
+(define test (term ((λ x (+ x (1 @ ,Nat))) 1)))
+
+;(traces Baseline-reduction test)
+(reduce test)
+
+;(redex-match? λCon-Baseline S test)
+;(redex-match? λCon-Baseline S (reduce test))
+;(redex-match? λCon M test)
+;(redex-match? λCon M (reduce test))
+
+;(done? test)
+;(done? (reduce test))
+
+
+;test
+(reduce test)
+(evaluate test)
+(evaluate (reduce test))
+(compare test)
+
+
+
+;; check canonicalize
+;;(define (step? M) (= (length (apply-reduction-relation canonicalize M)) 1)) 
+;;(redex-check Contracts M (if (not (canonical? (term M))) (step? (term M)) #t) #:attempts 100000)
+
+;(redex-check λCon M (compare (term M)) #:attempts 100000)
+
+;; tests
+;;(test-results)
+
