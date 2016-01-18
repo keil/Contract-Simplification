fc646f160aec80738fec380162f4da72afa7e044
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-18 10:33:04 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26219 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ test.baseline.rkt
@@ -0,0 +1,85 @@
+#lang racket
+(require redex)
+
+(require "lj.rkt")
+(require "lcon.rkt")
+(require "baseline.rkt")
+
+(require "examples.rkt")
+
+#|
+ _____       _      
+|_   _|__ __| |_ ___
+  | |/ -_|_-<  _(_-<
+  |_|\___/__/\__/__/
+                    
+|#
+
+;; Test: Simple Terms
+;; ==================
+
+(test-->>
+ Baseline-reduction
+ example:term/0
+ (term (((λ x (+ x 1)) 1) @ Num?)))
+
+(test-->>
+ Baseline-reduction
+ example:term/1
+ (term (((λ x (+ x 1)) @ (⊤ → Num?)) @ (⊤ → Pos?))))
+
+(test-->>
+ Baseline-reduction
+ example:term/2
+ (term ((((λ x (+ x 1)) 1) @ Num?) @ Pos?)))
+
+
+;; Test Unroll
+;; ===========
+
+(test-->
+ Baseline-reduction
+ example:unroll/1
+ (term ((λ x (+ 1 ((x @ (Num? → Num?)) 2))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ example:unroll/2
+ (term ((λ x (+ ((x @ (Num? → Num?)) 1) ((x @ (Num? → Num?)) 2))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ example:unfold/0
+ (term ((λ x ((x (2 @ Num?)) @ Num?)) (λ x (+ x 1)))))
+
+
+
+;; Test: Unfold
+;; ============
+
+(test-->
+ Baseline-reduction
+ example:unfold/1
+  (term ((λ x (+ 1 ((x (2 @ Num?)) @ Num?))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ example:unfold/2
+ (term ((λ x (+ ((x (1 @ Num?)) @ Num?) ((x @ (Num? → Num?)) 2))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ (term ((λ x (+ ((x (1 @ Num?)) @ Num?) ((x @ (Num? → Num?)) 2))) (λ x (+ x 1))))
+ (term ((λ x (+ ((x 1) @ Num?) ((x @ (Num? → Num?)) 2))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ (term ((λ x (+ ((x 1) @ Num?) ((x @ (Num? → Num?)) 2))) (λ x (+ x 1))))
+ (term ((λ x (+ ((x 1) @ Num?) ((x (2 @ Num?)) @ Num?))) (λ x (+ x 1)))))
+
+(test-->
+ Baseline-reduction
+ (term ((λ x (+ ((x 1) @ Num?) ((x (2 @ Num?)) @ Num?))) (λ x (+ x 1))))
+ (term ((λ x (+ ((x 1) @ Num?) ((x 2) @ Num?))) (λ x (+ x 1)))))
+
+(test-results)
---
+++ test.preevaluation.rkt
@@ -0,0 +1,98 @@
+#lang racket
+(require redex)
+
+(require "lj.rkt")
+(require "lcon.rkt")
+(require "baseline.rkt")
+
+(require "examples.rkt")
+
+#|
+ _____       _      
+|_   _|__ __| |_ ___
+  | |/ -_|_-<  _(_-<
+  |_|\___/__/\__/__/
+                    
+|#
+
+;; Test: Pre-evaluation
+;; ====================
+
+(test-->>
+ Pre-evaluation
+ (term ((1 @ Nat?) @ Nat?))
+ (term 1))
+
+(test-->>
+ Pre-evaluation
+ (term ((0 @ Nat?) @ Nat?))
+ (term 0))
+
+; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+; Note: Matthias Keil
+; Test are skipped until predicate handling is implemented
+; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+;(test-->> 
+; Pre-evaluation
+; (term (assert (assert 0 Pos?) Nat?)) (term +blmae))
+
+;(test-->> 
+; Pre-evaluation
+; (term ((0 @ Nat?) @ Pos?))
+; (term +blame))
+
+(test-->>
+ Pre-evaluation
+ (term ((λ x (+ x 1)) (1 @ Nat?)))
+ (term ((λ x (+ x 1)) 1)))
+
+(test-->>
+ Pre-evaluation
+ (term ((λ x (+ x (1 @ Nat?))) 1))
+ (term ((λ x (+ x 1)) 1)))
+
+(test-->>
+ Pre-evaluation
+ (term ((λ x (+ (x @ Nat?) 1)) 1))
+ (term ((λ x (+ (x @ Nat?) 1)) 1)))
+
+(test-->>
+ Pre-evaluation
+ (term (1 @ (Nat? → Nat?)))
+ (term 1))
+
+(test-->>
+ Pre-evaluation
+ (term (x @ (Nat? → Nat?)))
+ (term (x @ (Nat? → Nat?))))
+
+#|
+ _____       _      
+|_   _|__ __| |_ ___
+  | |/ -_|_-<  _(_-<
+  |_|\___/__/\__/__/
+                    
+|#
+
+(test-->>
+ Baseline-reduction
+ example:verify/0
+ (term ((λ x (+ 1 x)) 1)))
+
+(test-->>
+ Baseline-reduction
+ example:skip/0
+ (term (+ 1 1)))
+
+(test-->>
+ Baseline-reduction
+ example:skip/1
+ (term (+ 1 1)))
+
+(test-->
+ Baseline-reduction
+ example:unroll/0
+ (term ((λ x ((x @ (Num? → Num?)) 2)) (λ x (+ x 1)))))
+
+(test-results)
