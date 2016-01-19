5d2c225dc3253cb52a833d7c9647d38ef68bf94d
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-19 14:44:24 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26245 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- lcon.rkt
+++ lcon.rkt
@@ -20,6 +20,9 @@
 
 (define-extended-language λCon λJ
   
+  ;; Predicates
+  (P ⊤ (M ⇒ P))
+  
   ;; Contracts
   ;; ---------
   
@@ -30,11 +33,11 @@
   ((C D) I Q (C ∪ D) (I ∩ C))
   
   ; Immediate Contracts
-  ((I J) (flat M) named)
-
+  ((I J) (flat M) named (flat P ...))
+  
   ; Delayed Contracts
   ((Q R) (C → D) (x → C) (Q ∩ R))
-
+  
   
   
   ;; λCon Extention
@@ -77,12 +80,12 @@
   
   ;; Solution (context/ subject)
   (ω (B_0 ∘ B_1))
-
-
+  
+  
   
   ;; Predefined Predicates
   ;; ---------------------
-
+  
   ;; TODO, only for testing
   (named
    Any? ⊤
@@ -94,15 +97,65 @@
    Neg?
    Nat?
    )
-)
+  )
 
-;; Naive Subtyping of Contracts
-;; ============================
+;; Predicate Subset
+;; ================
+;(< #t #f) defined on real
+;(number? +nan.0)
+;(real? +nan.0)
+
+(define-metafunction λCon
+  ≤ : P P -> boolean
+  
+  [(≤ P ⊤) #t]
+  [(≤ P P) #t]
+  
+  [(≤ P (M ⇒ P)) #t]
+  
+  
+  [(≤ P any) #f])
+
+  ;; Predicates
+  ;(P ⊤ (M ⇒ P))
+
+
+(define-metafunction λCon
+  ∈ : P (flat P ...) -> boolean
+  [(∈ P_i (flat P_0 ... P_i P_i+1 ...)) #t] ;; subset
+  [(∈ P any) #f])
+
+(define-metafunction λCon
+  ∈x : P (flat P ...) -> boolean
+  [(∈x P_i (flat P_0 ... P_i P_i+1 ...)) #t]
+  [(∈x P any) #f])
+
+
+
+
+;; Naive Subsets of Contracts
+;; ==========================
+
+;; A contract C is subset of contract D iff
+;; C is more restrictive than D.
+
+;; If C ⊑ D then \forall .
+;; * V \in [[C]]+ => V \in [[D]]+
+;; * E \in [[C]]- => E \in [[D]]-
+;; resp.
+;; * V \not\in [[D]]+ => V \not\in [[C]]+
+;; * E \not\in [[D]]- => E \not\in [[C]]-
+
+;; It follows that:
+;;    E[[ M @ D ]] --> +blame/-blame
+;; => E[[ M @ C ]] --> +blame/-blame
+;; resp.
+;;    E[[ M @ C ]] --> V
+;; => E[[ M @ D ]] --> V
 
 (define-metafunction λCon
   ⊑ : C D -> boolean
-  [(⊑ C D) ,(and (term (⊑/context C D)) (term (⊑/subject C D)))]
-)
+  [(⊑ C D) ,(and (term (⊑/context C D)) (term (⊑/subject C D)))])
 
 (define-metafunction λCon
   ⊑/context : C D -> boolean
@@ -118,7 +171,7 @@
   [(⊑/context C_0 (C_1 ∪ D_1)) ,(and (term (⊑/context C_0 C_1)) (term (⊑/context C_0 D_1)))]
   ;; Dependent
   ;; TODO
-)
+  )
 
 (define-metafunction λCon
   ⊑/subject : C D -> boolean
@@ -145,7 +198,8 @@
   [(⊑/subject C_0 (C_1 ∪ D_1)) ,(or (term (⊑/subject C_0 C_1)) (term (⊑/subject C_0 D_1)))]
   ;; Dependent Contract
   ;; TODO
-)
+  )
+
 
 
 ;; Blame Calculation
@@ -222,58 +276,59 @@
 |#
 
 (define λCon-reduction
-  (extend-reduction-relation λJ-reduction
+  (extend-reduction-relation 
+   λJ-reduction
    λCon
-;   (--> (in-hole E (assert v C))
-;        (in-hole E (v @ C))
-;        "Assert"
-;   )
+   ;   (--> (in-hole E (assert v C))
+   ;        (in-hole E (v @ C))
+   ;        "Assert"
+   ;   )
    ;; Immediate Contarcts
    (--> (in-hole E (V @ (flat M)))
         (in-hole E (V @ (eval (M V))))
         "Flat"
-   )
+        )
    (--> (in-hole E (V @ (eval W)))
         (in-hole E V)
         "Unit"
         (side-condition (not (false? (term W))))
         ;(side-condition (not (equal? (term W) #f)))
-   )
+        )
    (--> (in-hole E (V @ (eval W)))
         (in-hole E V)
         ;;blame ;; TODO, Change to V and introduce top-level blame
         "Blame"
         (side-condition (false? (term W)))
-   )
+        )
    (--> (in-hole E (V @ (C ∪ D)))
         (in-hole E ((V @ C) @ D))
         "Union"
-   )
+        )
    (--> (in-hole E (V @ (I ∩ C)))
         (in-hole E ((V @ I) @ C))
         "Intersection"
-   )
+        )
    ;; Delayed Contarcts
    (--> (in-hole E ((V @ (C → D)) W))
         (in-hole E ((V (W @ C)) @ D))
         "D-Function"
-   )
+        )
    (--> (in-hole E ((V @ (x → C)) W)) ;; TODO
         (in-hole E ((V W) @ C))
         "D-Dependent"
-   )
+        )
    (--> (in-hole E ((V @ (Q ∩ R)) W))
         (in-hole E (((V @ Q) @ R) W))
         "D-Intersection"
-   )
-
-
+        )
+   
+   
    ;; Lookup
    (--> (in-hole E (V @ named))
         (in-hole E (V @ (lookup named)))
         "Lookup"
-   )
-))
+        )
+   ))
 
 (define-metafunction λCon
   lookup : named -> I
@@ -286,11 +341,11 @@
   [(lookup Num?) (flat (λ x (number? x)))]
   [(lookup Str?) (flat (λ x (string? x)))]
   [(lookup Bool?) (flat (λ x (boolean? x)))]
-
+  
   [(lookup Pos?) (flat (λ x (> x 0)))]
   [(lookup Nat?) (flat (λ x (or (> x 0) (= x 0))))]
   [(lookup Neg?) (flat (λ x (< x 0)))]
-)
+  )
 
 
 
