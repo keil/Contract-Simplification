bcc064138e4d5e929de887dc6bd6dbae001e5874
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-01-23 21:03:34 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26273 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- symbolic.rkt
+++ symbolic.rkt
@@ -172,13 +172,10 @@
 (test-->> Symbolic-reduction (term (((λ x (- x 1)) @ (Pos? → Pos?)) 1)) (term (0 Num? Pos?)))
 
 (test-->> Symbolic-reduction (term ((((λ x (λ y (+ x y))) @ (Pos? → (Pos? → Pos?))) 1) 1)) (term (2 Num? Pos?)))
-(traces Symbolic-reduction (term ((((λ x (λ y (+ x y))) @ (Pos? → (Pos? → Pos?))) 1) 1)))
-
+;(traces Symbolic-reduction (term ((((λ x (λ y (+ x y))) @ (Pos? → (Pos? → Pos?))) 1) 1)))
 
 (test-->> Symbolic-reduction (term ((λ x (x 1)) ((λ x (+ x 1)) @ (Pos? → Pos?)))) (term (2 Num? Pos?)))
-
 (test-->> Symbolic-reduction (term ((((λ y (λ x ((y x) 1))) @ ((Pos? → (Pos? → Pos?)) → (Pos? → Pos?))) (λ x (λ y (+ x y)))) 1)) (term (2 Num? Pos? Pos?)))
-
 (test-->> Symbolic-reduction (term ((λ x (x 1)) ((λ x (+ x 1)) @ ⊤))) (term (2 Num?)))
 ;(traces Symbolic-reduction (term ((λ x (x 1)) ((λ x (+ x 1)) @ ⊤))))
 
