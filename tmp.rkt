ca6c33fbacb6e089ddba0b203cd95724adab771d
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-02-04 21:05:45 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26384 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- tmp.rkt
+++ tmp.rkt
@@ -5,26 +5,29 @@
 (require "lcon.rkt")
 
 
+
 (test-->> 
  λCon-reduction 
  (term (· (((λ x (+ x 1)) @ ((flat %Positive) → (flat %Positive))) 0)))
- (term (((ι2 ◃ (#t ∘ #t)) ((ι1 ◃ (#t ∘ #f)) ((♭ ◃ (ι1 → ι2)) ·))) 1))) ;; TODO
+ (term (((ι2 ◃ (#t ∘ #t)) ((ι1 ◃ (#t ∘ #t)) ((♭ ◃ (ι1 → ι2)) ·))) 1))) ;; TODO
+
+
+(test-->> 
+ λCon-reduction 
+ (term (· (((λ x (+ x 1)) @ (((flat %Positive) → (flat %Boolean)) ∩ ((flat %Number) → (flat %Number)))) 1)))
+ (term (((ι2 ◃ (#t ∘ #t)) ((ι1 ◃ (#t ∘ #t)) ((♭ ◃ (ι1 → ι2)) ·))) 1))) ;; TODO
 
 
 (term
  (is-blame-state? ((ι2 ◃ (#t ∘ #f)) ((ι1 ◃ (#t ∘ #f)) ((♭ ◃ (ι1 → ι2)) ·))))
  )
 
-(term
- (is-false? (μ ((ι2 ◃ (#t ∘ #f)) ((ι1 ◃ (#t ∘ #t)) ((♭ ◃ (ι1 → ι2)) ·))) ι2))
- )
-
 
 (term
- (check-blame-state ((ι2 ◃ (#t ∘ #f)) ((ι1 ◃ (#t ∘ #f)) ((♭ ◃ (ι1 → ι2)) ·))) (♭1 ♭2 ♭))
+ (produce-blame ((ι2 ◃ (#t ∘ #f)) ((ι1 ◃ (#t ∘ #f)) ((♭ ◃ (ι1 → ι2)) ·))))
  )
 
-(not (term
- (is-blame-state? ·)))
+;(not (term
+; (is-blame-state? ·)))
 
 (test-results)
