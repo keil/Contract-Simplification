06cc309f277bea483744de021742a40ff3a61acd
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-02-04 15:02:47 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26381 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ old.rkt
@@ -0,0 +1,52 @@
+#lang racket
+
+  [(is-blame-state? ((♭ ◃ κ) ς)) (or (μ ((♭ ◃ κ) ς) ♭) (is-blame-state? ς))]
+  [(is-blame-state? ((ι ◃ κ) ς)) (is-blame-state? ς)]
+  [(is-blame-state? ·) #f])
+
+
+
+(define
+  (evaluate M)
+  (car (apply-reduction-relation* λCon-reduction M)))
+
+
+
+
+
+;(redex-match? λCon ♭ (term ♭1))
+;(variable-not-in (term (+ ♭1 ♭1)) (term ♭))
+;(variable-not-in (term (+ ι y)) (term ι))
+;(fresh (term (+ ι y)))
+
+(define λCon-value?
+  (redex-match? λCon V))
+
+
+
+
+(define-metafunction λCon
+  Σ : P -> (M ...)
+  [(Σ ⊤) ((λ x #t))]
+  [(Σ ⊥) ((λ x #f))]
+  ;[(Σ (⊤ / M)) (M)]
+  [(Σ (P / M)) (⊕ (Σ P) (M))]
+  [(Σ predefined) (Σ (lookup/ predefined))]
+  )
+
+
+(define-metafunction λCon
+  ⊕ : (M ...) (M ...) -> (M ...)
+  [(⊕ (M ...) ()) (M ...)]
+  [(⊕ () (M ...)) (M ...)]
+  [(⊕ (M_0 ... M_n M_i ...) (M_n M_m ...)) (⊕ (M_0 ... M_n M_i ...) (M_m ...))]
+  [(⊕ (M_0 ...) (M_n M_m ...)) (⊕ (M_0 ... M_n) (M_m ...))])
+
+
+
+
+      (in-hole E (V @ (eval ,(with-handlers 
+                                    ([(λ x #t) (lambda (exn) (term #f))])
+                                  (evaluate (term (M V))))))))
+
+(side-condition (not (false? (term W))))
