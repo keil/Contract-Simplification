af1c61b593c8e334da8083fb33090ab286fec8b8
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-02-17 16:26:48 +0000

    snapshot
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26456 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- tests.old/test.remain.rkt
+++ tests.old/test.remain.rkt
@@ -1,11 +1,9 @@
 #lang racket
 (require redex)
+(require rackunit)
 
-(require "lj.rkt")
-(require "lcon.rkt")
-(require "baseline.rkt")
+(require "../baseline.rkt")
 
-(require "examples.rkt")
 
 #|
  _____       _      
@@ -15,69 +13,45 @@
                     
 |#
 
-(test-->>
- Baseline-reduction
- example:remain/0
- (term ((λ x 1) @ (Num? → ⊤))))
-
-;(traces Baseline-reduction example:remain/1)
+;; Test Remining Contract
+;; ======================
 
 (test-->>
  Baseline-reduction
- example:remain/1
- (term ((λ x x) @ (Num? → Num?))))
+ (term (· ((λ x 1) @ (Number? → Number?))))
+ (term (· ((λ x 1) @ (Number? → Number?)))))
 
-;;XXX
-;(traces Baseline-reduction example:remain/1)
+(test-->>
+ Baseline-reduction
+ (term (· ((λ x x) @ (Number? → Number?))))
+ (term (· ((λ x x) @ (Number? → Number?)))))
 
 (test-->> 
  Baseline-reduction
- example:remain/2
- (term ((λ x x) @ ((Num? → Num?) → (Num? → Num?)))))
-
-;;XXX
-;(traces Baseline-reduction example:remain/2)
+ (term (· ((λ x x) @ ((Number? → Number?) → (Number? → Number?)))))
+ (term (· ((λ x x) @ ((Number? → Number?) → (Number? → Number?))))))
 
 (test-->> 
  Baseline-reduction
- example:remain/3
- (term (((λ f (f 1)) (λ x 1)) @ ⊤)))
-
-; XXX
-;(traces Baseline-reduction example:remain/3)
+ (term (· ((λ f (f 1)) ((λ x 1) @ (Number? → Number?)))))
+ (term (· (((λ f (f 1)) (λ x 1)) @ ⊤))))
 
 (test-->> 
  Baseline-reduction
- example:remain/4
- (term (((λ f (f #t)) (λ x 1)) @ ⊤)))
-
-; XXX
-;(traces Baseline-reduction example:remain/4)
-
+ (term (· ((λ f (f #t)) ((λ x 1) @ (Number? → Number?)))))
+ (term (· (((λ f (f #t)) (λ x 1)) @ ⊤))))
 
 (test-->> 
  Baseline-reduction
- example:remain/5
- (term (((λ f (f 1)) (λ x x)) @ Num?)))
-
-; XXX, did not reduce beacusse it is not a canonical contract
-;(traces Baseline-reduction example:remain/5)
+ (term (· ((λ f (f 1)) ((λ x x) @ (Number? → Number?)))))
+ (term (· (((λ f (f 1)) (λ x x)) @ Number?))))
 
 (test-->> 
  Baseline-reduction
- example:remain/6
- (term (((λ f (f #t)) (λ x x)) @ Num?)))
-
-; XXX
-;(traces Baseline-reduction example:remain/6)
-
+ (term (· ((λ f (f #t)) ((λ x x) @ (Number? → Number?)))))
+ (term (· (((λ f (f #t)) (λ x x)) @ Number?))))
 
 (test-->> 
  Baseline-reduction
- example:remain/7
- (term (λ f ((f (λ x x)) 1) ((λ x x) @ ((Num? → Num?) → (Num? → Num?))))))
-
-; XXX
-;(traces Baseline-reduction example:remain/7)
-
-(test-results)
+ (term (· (λ f ((f (λ x x)) 1) ((λ x x) @ ((Number? → Number?) → (Number? → Number?))))))
+ (term (· (λ f ((f (λ x x)) 1) ((λ x x) @ ((Number? → Number?) → (Number? → Number?)))))))
