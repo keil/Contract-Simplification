1b0bfb998843dd9b5ae868082e6015c10a048662
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-02-25 11:23:57 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26488 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- examples/example.addone.rkt
+++ examples/example.addone.rkt
@@ -17,15 +17,15 @@
 
 (define 
   example/addone/0
-  (term (· (((λ f (λ x ((f 1) x)))
-             (λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))))
-            1))))
+  (term (((λ f (λ x ((f 1) x)))
+          (λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))))
+         1)))
 
 ;; Notes
 ;; -----
 ;; Reduction steps: 10
 
-;(traces λCon-reduction example/addone/0)
+;(traces λCon-reduction (term (· ,example/addone/0)))
 
 
 
@@ -34,28 +34,34 @@
 
 (define 
   example/addone/1
-  (term (· (((λ f (λ x ((f 1) x)))
-             ((λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))) @ ♭ ((Number? → (Number? → Number?)) ∩ (String? → (String? → String?)))))
-            1))))
+  (term (((λ f (λ x ((f 1) x)))
+          ((λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))) @ ♭ ((Number? → (Number? → Number?)) ∩ (String? → (String? → String?)))))
+         1)))
 
 ;; Notes
 ;; -----
 ;; Reduction steps: 46
 
-;(traces λCon-reduction example/addone/1)
+;(traces λCon-reduction (term (· ,example/addone/1)))
 
 
 
 ;; # Sugar Reduction
 ;; -----------------
 
-(test-->>
- Baseline-reduction
- example/addone/1
- (term (((ι9 ◃ (#t ∘ #t)) ((ι7 ◃ (#t ∘ #t)) ((ι6 ◃ (ι9 → ι10)) ((ι3 ◃ (ι7 → ι8)) ((ι5 ◃ (#t ∘ #t)) ((ι2 ◃ (ι5 → ι6)) ((ι1 ◃ (ι3 ∩ ι4)) ((ι ◃ (ι1 → ι2)) ((♭ ◃ ι) ·))))))))) ((((λ f (λ x ((f x) 1))) (λ x (λ y (+ x y)))) @ ι10 (⊤ → Number?)) @ ι8 (Number? → ⊤)))))
+;(test-->>
+; Baseline-reduction
+; (term (· ,example/addone/1))
+; (term (((ι12 ◃ (#t ∘ #t)) ((ι13 ◃ (#t ∘ #t)) ((ι10 ◃ (ι13 → ι14)) ((ι11 ◃ (#t ∘ #t)) ((ι8 ◃ (ι11 → ι12)) ((ι9 ◃ (#t ∘ #t)) ((ι4 ◃ (ι9 → ι10)) ((ι7 ◃ (#t ∘ #t)) ((ι5 ◃ (ι7 → ι8)) ((ι3 ◃ (ι5 ∩ ι6)) ((ι2 ◃ (ι3 → ι4)) ((ι1 ◃ (#t ∘ #t)) ((ι ◃ (ι1 → ι2)) ((♭ ◃ ι) ·)))))))))))))) ((((λ f (λ x ((f 1) x))) (λ x (λ y (+ x y)))) 1) @ ι14 Number?))))
 
 ;; Notes
 ;; -----
-;; Reduction steps: 46
+;; Optimization steps: 23
 
-(traces Baseline-reduction example/addone/1)
+
+;; Notes
+;; -----
+;; Reduction steps: 11
+
+(traces Baseline-reduction (term (· ,example/addone/1)))
+(traces λCon-reduction (λCon~~>* (term (· ,example/addone/1))))
