f9986eeb5f34be0602e1cfdbdcc358e09f6905de
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-05-06 11:37:02 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26630 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ examples/example.addone.3.rkt
@@ -0,0 +1,61 @@
+#lang racket
+(require redex)
+
+(require "../lcon.rkt")
+;(require "../baseline.rkt")
+;(require "../lift.rkt")
+(require "../split.rkt")
+(require "../subset.rkt")
+
+(provide (all-defined-out))
+
+;; AddOne (intersection)
+;; =====================
+;; Motivating Example.
+
+
+
+;; # λJ (Reduction without contracts)
+;; ----------------------------------
+;; Reduction steps: 10
+
+(define 
+  example/addone/3
+  (term ((λ f (λ x ((f 1) x))) (λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))))))
+;(traces λCon-reduction (term (· (,example/addone/1 1))))
+
+
+
+;; # λCon (Reduction with contracts)
+;; ---------------------------------
+;; Reduction steps: 58
+
+(define 
+  example/addone/3/contracted
+  (term ((λ f ((λ x ((f 1) x)) @ ♭0 (Positive? → Positive?))) ((λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))) @ ♭1 ((Number? → (Number? → Number?)) ∩ (String? → (String? → String?)))))))
+;(traces λCon-reduction (term (· (,example/addone/1/contracted 1))))
+  
+
+
+;; # Sugar Reduction
+;; -----------------
+;; Optimization steps: 21
+;; Reduction steps:    35
+
+;(traces Baseline-reduction (term (· ,example/addone/1/contracted)))
+
+;(let ([configuration (λCon~~>* (term (· ,example/addone/1/contracted)))]) 
+;  (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
+
+
+
+;; # Sugar Reduction
+;; -----------------
+;; Optimization steps: 40
+;; Reduction steps:    36
+
+(traces Subset-reduction (term (· ,example/addone/3/contracted)))
+
+
+;(let ([configuration (λCon/Subset~~>* (term (· ,example/addone/3/contracted)))]) 
+;  (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
