f7a0b1e1bee17a5f17d774abb0ffb47f24768ec9
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-06-01 09:38:01 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26740 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- examples/example.addone.1.rkt
+++ examples/example.addone.1.rkt
@@ -43,7 +43,7 @@
 ;; Optimization steps: 21
 ;; Reduction steps:    33
 
-;(traces Baseline-reduction (term (· ,example/addone/1/contracted)))
+(traces Baseline-reduction (term (· ,example/addone/1/contracted)))
 
 (let ([configuration (λCon/Baseline~~>* (term (· ,example/addone/1/contracted)))]) 
   (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
