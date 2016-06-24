900bea47efa2bb7c054c83df208934ad99689f81
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-06-24 12:55:13 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/Sugar@26844 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ examples/example.addone.4.rkt
@@ -0,0 +1,80 @@
+#lang racket
+(require redex)
+
+(require "../lcon.rkt")
+(require "../baseline.rkt")
+(require "../subset.rkt")
+(require "../join.rkt")
+(require "../success.rkt")
+
+(provide (all-defined-out))
+
+;; AddOne (two contracts, intersection)
+;; ====================================
+;; Motivating Example.
+
+
+
+;; # λJ (Reduction without contracts)
+;; ----------------------------------
+;; Reduction steps: 10
+
+(define 
+  example/addone/4
+  (term ((λ f (λ x ((f 1) x))) (λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))))))
+
+;(traces λCon-reduction (term (· (,example/addone/4 1))))
+
+
+
+;; # λCon (Reduction with contracts)
+;; ---------------------------------
+;; Reduction steps: 72
+
+(define 
+  example/addone/4/contracted
+  (term ((λ f ((λ x ((if (string? x) (f "1") (f 1)) x)) @ ♭0 ((Positive? → Positive?) ∩ (String? → String?)))) ((λ x (λ y (if (or (string? x) (string? y)) (string-append x y) (+ x y)))) @ ♭1 ((Number? → (Number? → Number?)) ∩ (String? → (String? → String?)))))))
+
+;(traces λCon-reduction (term (· (,example/addone/4/contracted 1))))
+
+;; Make another version that contains the second application in if
+
+;; # Baseline Reduction
+;; --------------------
+;; Optimization steps: 32
+;; Reduction steps:    58
+
+;(traces Baseline-reduction (term (· ,example/addone/4/contracted)))
+
+;(let ([configuration (λCon/Baseline~~>* (term (· ,example/addone/4/contracted)))]) 
+;  (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
+
+
+
+;; # Subset Reduction
+;; ------------------
+;; Optimization steps: xx
+;; Join Steps:         xx
+;; Reduction steps:    xx
+
+(traces Subset-reduction (term (· ,example/addone/4/contracted)))
+
+;(traces Join-reduction (λCon/Subset~~>* (term (· ,example/addone/4/contracted))))
+
+;(let ([configuration (λCon/Join~~>* (λCon/Subset~~>* (term (· ,example/addone/4/contracted))))]) 
+;  (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
+
+
+
+;; # Success Reduction
+;; -------------------
+;; Optimization steps: xx
+;; Join Steps:         xx
+;; Reduction steps:    xx
+
+;(traces Success-reduction (term (· ,example/addone/4/contracted)))
+
+;(traces Join-reduction (λCon/Subset~~>* (term (· ,example/addone/4/contracted))))
+
+;(let ([configuration (λCon/Join~~>* (λCon/Success~~>* (term (· ,example/addone/4/contracted))))]) 
+;  (traces λCon-reduction (term ((⇓/State ,configuration) ((⇓/Term ,configuration) 1)))))
